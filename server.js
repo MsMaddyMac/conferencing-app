c18a995e743678f6b45a0a70d1a6056244d40f34
Madeline McIntosh <mmcintosh2k14@gmail.com>
2020-12-05 09:49:27 -0500

    sets up socket connection

--- server.js
+++ server.js
@@ -7,3 +7,8 @@ const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.log("Listening on port " + PORT)
 })
+
+// Set up socket
+io.on('connection', function(socket){
+  console.log("client is connected" + socket.id)
+})
