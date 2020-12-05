36689356b4f5e4cecf6d1d32847665b7b5fea145
Madeline McIntosh <mmcintosh2k14@gmail.com>
2020-12-05 11:48:19 -0500

    adds video id exchange

--- public/client.js
+++ public/client.js
@@ -32,3 +32,93 @@ const message = document.getElementById('message'),
       socket.on('userTyping', (data) => {
         typing.innerHTML = '<p> <em>' + data + ' is typing...</em></p>';
       });
+
+      // uses peer for signaling server for video chat
+      function getLVideo(callbacks) {
+        navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+        var constraints = {
+          audio: true,
+          video: true
+        }
+        navigator.getUserMedia(constraints, callbacks.success, callbacks.error)
+      }
+
+      function recStream(stream, elemid) {
+        var video = document.getElementById(elemid);
+
+        video.srcObject = stream;
+
+        window.peer_stream = stream;
+      }
+
+      // get the local video and display it with permission
+      getLVideo({
+        success: function(stream){
+          window.localstream = stream;
+          recStream(stream, 'lVideo');
+        },
+        error: function(err){
+          alert('cannot access your camera');
+          console.log(err);
+        }
+      })
+
+      var conn;
+      var peer_id;
+
+      // create a peer connection with peer object
+      var peer = new Peer();
+
+      // display the peer ID on the DOM
+      peer.on('open', function() {
+        document.getElementById('displayID').innerHTML = peer.id
+      });
+
+      peer.on('connection', function(connection){
+        conn = connection;
+        peer_id = connection.peer;
+
+        document.getElementById('connId').value = peer_id;
+      });
+
+      peer.on('error', function(err){
+        alert('an error has taken place:' + err);
+        console.log(err);
+      })
+      // onClick with connection button and exchange peer ID's (expose ICE info)
+      document.getElementById('conn_button').addEventListener('click', function(){
+        peer_id = document.getElementById('connId').value;
+
+        if(peer_id){
+          conn = peer.connect(peer_id)
+        } else{
+          alert('enter an ID');
+          return false;
+        }
+      })
+
+      // call onClick sends offer and answer is exchanged
+      peer.on('call', function(call){
+        var acceptCall = confirm('Do you want to answer this call?');
+
+        if(acceptCall){
+          call.answer(window.localstream);
+
+          call.on('stream', function(stream){
+            window.peer_stream = stream;
+
+            recStream(stream, 'rVideo')
+          });
+
+          call.on('close', function(){
+            alert('The call has ended.');
+          })
+        } else{
+          console.log('Call denied.')
+        }
+      });
+
+      // ask to call
+      // accept call
+      // display the remote and local video on the clients
+
